34f31d99cdb4de4802169492f1dd4df2cdeea7f6
LázaroK <lazarolk23@hotmail.com>
2024-11-25 14:43:43 -0300

    Service de DepositoBanco para Enviar dados do Deposito para fila e persistir

--- Mensageria_RabbitMQ/msdeposito/src/main/java/com/pblazaro/msdeposito/Application/DepositoBancoService.java
+++ Mensageria_RabbitMQ/msdeposito/src/main/java/com/pblazaro/msdeposito/Application/DepositoBancoService.java
@@ -20,8 +20,50 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class DepositoBancoService {
 
+    private final DepositoBancoPublisher depositoBancoPublisher;
+    private final DepositoRepository depositoRepository;
+
     public static boolean verificandoConexaoRabbitmq(RabbitTemplate rabbitTemplate){
         return rabbitTemplate.isRunning();
+    }
+
+    @Transactional
+    public DepositoBanco efetuarDepositoBancario(DadosParaDeposito dadosDoDeposito) {
+        try {
+            DepositoBanco depositoBanco = new DepositoBanco();
+            depositoBanco.setIdContaCliente(dadosDoDeposito.getIdContaCliente());
+            depositoBanco.setValorDeposito(dadosDoDeposito.getValorDeposito());
+
+            BigDecimal valorDoDeposito = dadosDoDeposito.getValorDeposito();
+            if (depositoValido(valorDoDeposito) == false) {
+                log.warn("PROCESSANDO DEPÓSITO INVÁLIDO");
+                depositoBanco.setStatus(StatusDeposito.ERRO);
+            } else {
+                log.warn("PROCESSANDO DEPÓSITO VÁLIDO");
+                depositoBanco.setStatus(StatusDeposito.DEPOSITADO);
+            }
+
+            depositoRepository.save(depositoBanco);
+            log.info("DEPÓSITO DE Nº: {} REGISTRADO!", depositoBanco.getIdDeposito());
+
+            depositoBancoPublisher.EnviandoDepositoParaEfetuarTransacao(depositoBanco);
+            log.info("MENSAGEM ENVIADA COM SUCESSO - DEPÓSIDO ENVIADO PARA EXTRATO");
+            return depositoBanco;
+        } catch (NumberFormatException e) {
+            log.error("FALHA AO REALIZAR DEPOSITO");
+            throw new NumberFormatException();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("NÃO FOI POSSÍVEL CONVERTER PARA JSON");
+        }
+    }
+
+    public boolean depositoValido(BigDecimal valorDeposito) {
+        String stringValor = String.valueOf(valorDeposito);
+        Double doubleValor = Double.parseDouble(stringValor);
+        if (doubleValor <= 0) {
+            return false;
+        }
+        return true;
     }
 
 
